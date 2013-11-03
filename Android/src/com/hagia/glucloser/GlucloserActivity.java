4a4de6d5b6930033785ec4caa48da86e93f2b21b
Nathan Lefler <lefler@amazon.com>
2013-11-02 18:30:27 -0700

    Use sliding menu

--- Android/src/com/hagia/glucloser/GlucloserActivity.java
+++ Android/src/com/hagia/glucloser/GlucloserActivity.java
@@ -2,25 +2,25 @@ package com.hagia.glucloser;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentManager.OnBackStackChangedListener;
 import android.app.FragmentTransaction;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.content.res.Configuration;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.SpinnerAdapter;
+import android.widget.ListView;
 
 import com.crashlytics.android.Crashlytics;
 import com.hagia.glucloser.fragments.add.AddMealFragment;
@@ -35,78 +35,65 @@ import com.hagia.glucloser.util.database.save.SaveManager;
 import com.parse.Parse;
 
 public class GlucloserActivity extends Activity {
-	private static final String LOG_TAG = "Pump_Pump";
+	private static final String LOG_TAG = "Glucloser_MainActivity";
 	public static final int LOG_LEVEL = Log.VERBOSE;
 
+    // TODO: Why do I need this
 	private static GlucloserActivity instance;
-	private ActionBar actionBar;
-	private Bundle fragmentArgs;
-	private Handler handler;
 
-	public enum NavigationItem {
+	private ActionBar _actionBar;
+    private DrawerLayout _drawerLayout;
+    private ActionBarDrawerToggle _drawerToggle;
+    private ListView _drawerList;
+
+	private enum NavigationItem {
 		HomeItem,
 		AddMealItem,
 		HistoryItem,
 		StatsItem
 	}
-	private static String[] navigationItemNames;
 
-
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		
+        super.onCreate(savedInstanceState);
+
 		if (instance == null) {
 			instance = this;
 		}
 
 		setContentView(R.layout.main);
-		navigationItemNames = getResources().getStringArray(R.array.action_bar_action_list);
 
-		handler = new Handler();
+		String[] navigationItemNames = getResources().getStringArray(R.array.action_bar_action_list);
+        _drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        _drawerToggle = new ActionBarDrawerToggle(this, _drawerLayout, R.drawable.ic_drawer,
+                R.string.drawer_open_action, R.string.drawer_close_action);
+        _drawerLayout.setDrawerListener(_drawerToggle);
+        _drawerList = (ListView) findViewById(R.id.drawer_list);
+
+        _drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navigationItemNames));
+        _drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        _actionBar = getActionBar();
+        _actionBar.setDisplayHomeAsUpEnabled(true);
+        _actionBar.setHomeButtonEnabled(true);
 
         // Initialize crash reporting
         Crashlytics.start(this);
 
-		// Initialize parse
 		Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_api_key));
 
-		// Initialize Database
 		DatabaseUtil.initialize(this);
 
-		// Initialize Location services
 		LocationUtil.initialize(
 				(LocationManager) this.getSystemService(Context.LOCATION_SERVICE),
 				this.getApplicationContext());
 		
 		SaveManager.initialize();
 
-		actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-		actionBar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
-
-		SpinnerAdapter spinnerAdapter = ArrayAdapter.createFromResource(this, 
-				R.array.action_bar_action_list, android.R.layout.simple_spinner_dropdown_item);
-		actionBar.setListNavigationCallbacks(spinnerAdapter, new ActionBar.OnNavigationListener() {
-			@Override
-			public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-				boolean success = internalSelectNavigationItemWithBundle(
-						getNavigationItemForPosition(itemPosition), fragmentArgs);
-				fragmentArgs = null;
-
-				return success;
-			}
-		});
-
-		if (savedInstanceState != null) {
-			actionBar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
-		}
-				
 		getFragmentManager().addOnBackStackChangedListener(new OnBackStackChangedListener() {
 			
 			@Override
 			public void onBackStackChanged() {
-				// Update Action Bar
 				if (getFragmentManager().getBackStackEntryCount() == 0) {
 					finish();
 				}
@@ -117,26 +104,30 @@ public class GlucloserActivity extends Activity {
 		// it serves no purpose.
 		//DatabaseUtil.instance().startNetworkSyncServiceUsingContext(this);
 		
-		super.onCreate(savedInstanceState);
 	}
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        _drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        _drawerToggle.onConfigurationChanged(newConfig);
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.layout.pump_menu, menu);
+		inflater.inflate(R.layout.glucloser_main_activity_options_menu, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
-	protected void onPause() {
-		Log.i(LOG_TAG, "Pause");
-
-		super.onPause();
-	}
-
-	@Override
 	protected void onStop() {
-		Log.i(LOG_TAG, "Stop");
 		LocationUtil.shutdown();
 		DatabaseUtil.syncIfNeeded();
 
@@ -145,20 +136,11 @@ public class GlucloserActivity extends Activity {
 
 	@Override
 	protected void onResume() {
-		Log.i(LOG_TAG, "Resume");
-
 		LocationUtil.initialize(
 				(LocationManager) this.getSystemService(Context.LOCATION_SERVICE),
 				this.getApplicationContext());
 
 		super.onResume();
-	}
-
-	@Override
-	protected void onDestroy() {
-		Log.i(LOG_TAG, "Destroy");
-
-		super.onDestroy();
 	}
 
 	public static GlucloserActivity getPumpActivity() {
@@ -167,53 +149,19 @@ public class GlucloserActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (_drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
 		switch (item.getItemId()) {
 		case R.id.sync:
-			DatabaseUtil.instance().startNetworkSyncServiceUsingContext(GlucloserActivity.getPumpActivity().getApplicationContext());
+			DatabaseUtil.instance().startNetworkSyncServiceUsingContext(this.getApplicationContext());
 			return true;
-        case R.id.edit_places:
-            showFragment(new EditPlacesFragment(), "Edit Places");
-            return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
-	}
-	
-	public void selectNavigationItemWithBundle(NavigationItem item,
-			Bundle bundle) {
-		int navigationPosition = 0;
-
-		switch (item) {
-		case HomeItem:
-			navigationPosition = 0;
-			break;
-		case AddMealItem:
-			navigationPosition = 1;
-			break;
-		case HistoryItem:
-			navigationPosition = 2;
-			break;
-		case StatsItem:
-			navigationPosition = 3;
-			break;
-		default:
-			navigationPosition = 0;
-		}
-
-		fragmentArgs = bundle;
-		actionBar.setSelectedNavigationItem(navigationPosition);
-	}
-
-	public void showFragment(Fragment fragment, String tag) {
-		if (tag == null) {
-			tag = "NOTAG";
-		}
-
-		FragmentTransaction transaction = getFragmentManager().beginTransaction();
-		transaction.replace(R.id.main_fragment_container, 
-				fragment, tag);
-		transaction.addToBackStack(tag); 
-		transaction.commit();
 	}
 
 	public void popFragmentStack() {
@@ -221,83 +169,24 @@ public class GlucloserActivity extends Activity {
 		manager.popBackStack();
 	}
 	
-	public void showModalMessage(final String title, final String message) {
-		handler.post(new Runnable() {
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            Fragment fragment = null;
+            String tag = "tag";
 
-			@Override
-			public void run() {
-				AlertDialog.Builder builder = new AlertDialog.Builder(GlucloserActivity.this);
+            fragment = new HomeFragment();
+            fragment = new AddMealFragment();
+            fragment = new HistoryFragment();
+            fragment = new StatsFragment();
+            fragment = new EditPlacesFragment();
 
-				builder.setTitle(title);
-				builder.setMessage(message);
-				builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-					}
-
-				});
-
-				AlertDialog alert = builder.create();
-				alert.show();
-			}
-			
-		});
-	}
-
-	private boolean internalSelectNavigationItemWithBundle(NavigationItem item,
-			Bundle bundle) {
-		Fragment fragment = null;
-		int navigationPosition = 0;
-
-		switch (item) {
-		case HomeItem:
-			fragment = new HomeFragment();
-			navigationPosition = 0;
-			break;
-		case AddMealItem:
-			fragment = new AddMealFragment();
-			navigationPosition = 1;
-			break;
-		case HistoryItem:
-			fragment = new HistoryFragment();
-			navigationPosition = 2;
-			break;
-		case StatsItem:
-			fragment = new StatsFragment();
-			navigationPosition = 3;
-			break;
-		default:
-			fragment = new HomeFragment();
-			navigationPosition = 0;
-		}
-
-		if (bundle != null) {
-			fragment.setArguments(bundle);
-		}
-
-		showFragment(fragment, navigationItemNames[navigationPosition]);
-
-		return true;
-	}
-
-	private NavigationItem getNavigationItemForPosition(int position) {
-		switch (position) {
-		case 0:
-			// Home
-			return NavigationItem.HomeItem;
-		case 1:
-			// Add Meal
-			return NavigationItem.AddMealItem;
-		case 2:
-			// History
-			return NavigationItem.HistoryItem;
-		case 3:
-			// Stats
-			return NavigationItem.StatsItem;
-		default:
-			return NavigationItem.HomeItem;
-		}
-	}
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.main_fragment_container,
+                    fragment, tag);
+            transaction.addToBackStack(tag);
+            transaction.commit();
+        }
+    }
 }
+
