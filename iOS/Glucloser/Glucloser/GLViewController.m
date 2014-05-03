7e4bcfd071216ec4c7ceaecd544fb29a4cd6c56b
Nathan Lefler <nathan.lefler@mlb.com>
2014-05-02 20:24:50 -0400

    Initial iOS commit

---
+++ iOS/Glucloser/Glucloser/GLViewController.m
@@ -0,0 +1,29 @@
+//
+//  GLViewController.m
+//  Glucloser
+//
+//  Created by Nathan Lefler on 5/2/14.
+//  Copyright (c) 2014 NLefler. All rights reserved.
+//
+
+#import "GLViewController.h"
+
+@interface GLViewController ()
+
+@end
+
+@implementation GLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iOS/Glucloser/GlucloserTests/GlucloserTests.m
@@ -0,0 +1,34 @@
+//
+//  GlucloserTests.m
+//  GlucloserTests
+//
+//  Created by Nathan Lefler on 5/2/14.
+//  Copyright (c) 2014 NLefler. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GlucloserTests : XCTestCase
+
+@end
+
+@implementation GlucloserTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
