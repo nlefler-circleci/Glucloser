8a5899777377cbee7dbc50d66441ba8a7bd46156
Nathan Lefler <nathan.lefler@mlb.com>
2014-12-26 17:39:16 -0500

    Super basic meal history on main activity

--- Android/Glucloser/app/src/main/java/com/nlefler/glucloser/MainActivity.java
+++ Android/Glucloser/app/src/main/java/com/nlefler/glucloser/MainActivity.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.nlefler.glucloser.dataSource.MealHistoryRecyclerAdapter;
+import com.nlefler.glucloser.models.Meal;
 import com.parse.ParseAnalytics;
+
+import java.util.ArrayList;
+
+import io.realm.Realm;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -63,6 +74,11 @@ public class MainActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        private static String LOG_TAG = "PlaceholderFragment";
+
+        private RecyclerView mealHistoryListView;
+        private RecyclerView.LayoutManager mealHistoryLayoutManager;
+        private MealHistoryRecyclerAdapter mealHistoryAdapter;
 
         public PlaceholderFragment() {
         }
@@ -71,7 +87,26 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            this.mealHistoryListView = (RecyclerView)rootView.findViewById(R.id.meal_history_list);
+
+            this.mealHistoryLayoutManager = new LinearLayoutManager(getActivity());
+            this.mealHistoryListView.setLayoutManager(this.mealHistoryLayoutManager);
+
+            this.mealHistoryAdapter = new MealHistoryRecyclerAdapter(new ArrayList<Meal>());
+            this.mealHistoryListView.setAdapter(this.mealHistoryAdapter);
+
+            updateMealHistory();
+
             return rootView;
+        }
+
+        private void updateMealHistory() {
+            Realm realm = Realm.getInstance(getActivity());
+            RealmQuery<Meal> query = realm.where(Meal.class);
+            RealmResults<Meal> results = query.findAll();
+            Log.d(LOG_TAG, "Found meals " + results.size());
+            this.mealHistoryAdapter.setMeals(results);
         }
     }
 }
