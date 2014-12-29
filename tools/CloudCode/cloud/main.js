58d726fc151dade89c4a0fb01c0e7118fd8377db
Nathan Lefler <nathan.lefler@mlb.com>
2014-12-29 13:23:58 -0500

    Send push on Foursquare check-in
    Broadcast to all and shove 4sq data in there

--- tools/CloudCode/cloud/main.js
+++ tools/CloudCode/cloud/main.js
@@ -7,6 +7,26 @@ app.use(express.bodyParser());  // Populate req.body
 app.post('/foursquareCheckin',
          function(req, res) {
   console.log(req.body);
+
+  Parse.Push.send({
+	  channels: [ "foursquareCheckin" ],
+	  data: {
+	    alert: "You checked in",
+	    uri: "com.nlefler.glucloser://logMeal",
+	    checkinData: req.body
+	  }
+	}, {
+	  success: function() {
+	    // Push was successful
+	    console.log("pushed");
+	  },
+	  error: function(error) {
+	    // Handle error
+	    console.log("push failed");
+	    console.log("" + error);
+	  }
+	});
+
   res.send(200);
 });
  
