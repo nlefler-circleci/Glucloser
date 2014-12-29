699654c231121f236f620c293b49e34568769d5d
Nathan Lefler <nathan.lefler@mlb.com>
2014-12-29 16:09:25 -0500

    Updates to cloud code
    Get 4sq user id and venue name
    Only send push to the user that checked in

--- tools/CloudCode/cloud/main.js
+++ tools/CloudCode/cloud/main.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
+
+function getFoursquareCheckInData(body) {
+	return JSON.parse(body.checkin);
+}
+
+function getFoursquareUserId(checkinData) {
+	return checkinData.user.id || false;
+}
  
+function getFoursquareVenueName(checkinData) {
+	return checkinData.venue.name || false;
+}
+
 // Global app configuration section
 app.use(express.bodyParser());  // Populate req.body
  
@@ -8,26 +20,51 @@ app.post('/foursquareCheckin',
          function(req, res) {
   console.log(req.body);
 
-  Parse.Push.send({
-	  channels: [ "foursquareCheckin" ],
-	  data: {
-	    alert: "You checked in",
-	    uri: "com.nlefler.glucloser://logMeal",
-	    checkinData: req.body
-	  }
-	}, {
-	  success: function() {
-	    // Push was successful
-	    console.log("pushed");
-	  },
-	  error: function(error) {
-	    // Handle error
-	    console.log("push failed");
-	    console.log("" + error);
-	  }
-	});
+  var checkinData = getFoursquareCheckInData(req.body);
+  if (!checkinData) {
+  	console.log("No checkin data");
+  	res.send(400);
+  	return;
+  }
 
-  res.send(200);
+  var foursquareUserId = getFoursquareUserId(checkinData);
+  if (!foursquareUserId) {
+  	console.log("Cant get foursquare user id");
+  	res.send(400);
+  	return;
+  } else {
+  	console.log("Foursquare user id " + foursquareUserId)
+  }
+
+  var notifTitle = "You checked in";
+  var venueName = getFoursquareVenueName(checkinData);
+  if (venueName) {
+  	notifTitle += " at " + venueName;
+  }
+  notifTitle += ". Log a meal?";
+
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo('channels', 'foursquareCheckin');
+  query.equalTo('foursquareUserId', foursquareUserId);
+
+  Parse.Push.send({
+  		where: query,
+		data: {
+		alert: notifTitle,
+		uri: "com.nlefler.glucloser://logMeal",
+		checkinData: checkinData
+		}
+	}, {
+		success: function() {
+			console.log("Pushed");
+			res.send(200);
+		},
+		error: function(error) {
+			console.log("Push failed");
+			console.log(JSON.stringify(error));
+			res.send(500);
+		}
+	});
 });
  
 app.listen();
