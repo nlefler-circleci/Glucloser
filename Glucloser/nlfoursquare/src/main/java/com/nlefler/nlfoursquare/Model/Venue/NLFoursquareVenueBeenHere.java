9f65f7181124818c53ad2639ca4ff73f2ae8908f
Nathan Lefler <n.lefler@gmail.com>
2015-05-10 21:42:07 -0700

    Remove nlfoursquare

--- Glucloser/nlfoursquare/src/main/java/com/nlefler/nlfoursquare/Model/Venue/NLFoursquareVenueBeenHere.java
+++
@@ -1,8 +0,0 @@
-package com.nlefler.nlfoursquare.Model.Venue;
-
-/**
- * Created by Nathan Lefler on 5/25/14.
- */
-public class NLFoursquareVenueBeenHere {
-    public int count;
-}
--- Glucloser/nlfoursquare/src/main/java/com/nlefler/nlfoursquare/Model/Venue/NLFoursquareVenueTips.java
+++
@@ -1,9 +0,0 @@
-package com.nlefler.nlfoursquare.Model.Venue;
-
-/**
- * Created by Nathan Lefler on 5/25/14.
- */
-public class NLFoursquareVenueTips {
-    // TODO: Fields
-    public int count;
-}
