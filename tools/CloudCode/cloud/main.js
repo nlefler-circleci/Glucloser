d424b00c7e4c4864b8bb2b6a77aa7e2fd902ff80
Nathan Lefler <n.lefler@gmail.com>
2015-08-09 15:20:10 -0400

    Update to basal jobs. Reset master key

--- tools/CloudCode/cloud/main.js
+++ tools/CloudCode/cloud/main.js
@@ -1,9 +1,11 @@
 var foursquare = require('cloud/foursquare.js');
 var cgm = require('cloud/cgm.js');
 var bolus = require('cloud/bolus.js');
+var basal = require('cloud/basal.js');
 var _ = require('underscore');
 var averages_job = require('cloud/averages_job.js');
 var prune_cgm_job = require('cloud/prune_cgm_job.js');
+var aggregate_basal_job = require('cloud/aggregate_basal_job.js');
 
 foursquare.foursquarePushHandler.listen();
 
@@ -16,3 +18,4 @@ Parse.Cloud.afterSave('Snack', function(request) {
 
 averages_job.RegisterPostBolusAveragesJob();
 prune_cgm_job.RegisterPruneCGMJob();
+aggregate_basal_job.RegisterAggregateBolusRates();
