491ba6ebe1d3d0fc5f331e23469589b67bc104f9
Nathan Lefler <n.lefler@gmail.com>
2015-09-30 18:44:08 -0400

    Fix unit tests

---
+++ Glucloser/app/src/androidTest/java/com/nlefler/glucloser/BolusPatternFactoryTests.kt
@@ -0,0 +1,12 @@
+package com.nlefler.glucloser
+
+import android.test.AndroidTestCase
+
+/**
+ * Created by nathan on 9/23/15.
+ */
+public class BolusPatternFactoryTests: AndroidTestCase() {
+    public fun testA() {
+        assert(false)
+    }
+}
