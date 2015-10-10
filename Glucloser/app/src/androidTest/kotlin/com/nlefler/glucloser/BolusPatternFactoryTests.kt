b8cb67858eb4852fc2b9736e9a247f12c36ba8a7
Nathan Lefler <n.lefler@gmail.com>
2015-10-10 18:22:12 -0400

    Gradle and project updates

---
+++ Glucloser/app/src/androidTest/kotlin/com/nlefler/glucloser/BolusPatternFactoryTests.kt
@@ -0,0 +1,17 @@
+package com.nlefler.glucloser
+
+import org.junit.Test
+import junit.framework.TestCase
+import kotlin.test.*
+import org.junit.After
+import org.junit.Before
+
+/**
+ * Created by nathan on 9/23/15.
+ */
+public class BolusPatternFactoryTests {
+
+    Test public fun testA() {
+        assert(false)
+    }
+}
